90055d4b0d9366114d4bb6c9495b7cd997975f64
Alun Thomas Evans - <alunthomas.evans@salle.url.edu>
2019-02-01 15:32:47 +0100

    First commit

---
+++ src/GraphicsUtilities.cpp
@@ -0,0 +1,116 @@
+#include "GraphicsUtilities.h"
+
+// ****** GEOMETRY ***** //
+
+//generates buffers in VRAM
+Geometry::Geometry(std::vector<float>& vertices, std::vector<float>& uvs, std::vector<float>& normals, std::vector<unsigned int>& indices) {
+	createVertexArrays(vertices, uvs, normals, indices);
+}
+
+void Geometry::render() {
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, num_tris * 3, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
+
+void Geometry::createVertexArrays(std::vector<float>& vertices, std::vector<float>& uvs, std::vector<float>& normals, std::vector<unsigned int>& indices) {
+	//generate and bind vao
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	GLuint vbo;
+	//positions
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &(vertices[0]), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//texture coords
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(float), &(uvs[0]), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	//normals
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(float), &(normals[0]), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//indices
+	GLuint ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &(indices[0]), GL_STATIC_DRAW);
+	//unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//set number of triangles
+	num_tris = (GLuint)indices.size() / 3;
+
+	//set AABB
+	setAABB(vertices);
+}
+
+
+// Given an array of floats (in sets of three, representing vertices) calculates and
+// sets the AABB of a geometry
+void Geometry::setAABB(std::vector<GLfloat>& vertices) {
+	//set very max and very min
+	float big = 1000000.0f;
+	float small = -1000000.0f;
+	lm::vec3 min(big, big, big);
+	lm::vec3 max(small, small, small);
+
+	//for all verts, find max and min
+	for (size_t i = 0; i < vertices.size(); i += 3) {
+		float x = vertices[i];
+		float y = vertices[i + 1];
+		float z = vertices[i + 2];
+
+		if (x < min.x) min.x = x;
+		if (y < min.y) min.y = y;
+		if (z < min.z) min.z = z;
+
+		if (x > max.x) max.x = x;
+		if (y > max.y) max.y = y;
+		if (z > max.z) max.z = z;
+	}
+	//set center and halfwidth based on max and min
+	aabb.center = lm::vec3((min.x + max.x) / 2,
+		(min.y + max.y) / 2,
+		(min.z + max.z) / 2);
+	aabb.half_width = lm::vec3(max.x - aabb.center.x,
+		max.y - aabb.center.y,
+		max.z - aabb.center.z);
+}
+
+//creates a standard plane geometry and return its
+int Geometry::createPlaneGeometry() {
+
+	std::vector<GLfloat> vertices, uvs, normals;
+	std::vector<GLuint> indices;
+	vertices = { -1.0f, -1.0f, 0.0f,    1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, -1.0f, 1.0f, 0.0f };
+	uvs = { 0.0f, 0.0f,   1.0f, 0.0f,    1.0f, 1.0f,   0.0f, 1.0f };
+	normals = { 0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f };
+	indices = { 0, 1, 2, 0, 2, 3 };
+
+	//generate the OpenGL buffers and create geometry
+	createVertexArrays(vertices, uvs, normals, indices);
+
+	return 1;
+}
+
+void Framebuffer::bindAndClear() {
+	glViewport(0, 0, width, height);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Framebuffer::initColor(GLsizei w, GLsizei h) {
+
+	width = w; height = h;
+
+	
+}
